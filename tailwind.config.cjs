a9a2c2a5cb6f0334d45872f900693d01c68e71af
Alfayed Dennita <alfayeddennita@gmail.com>
2022-11-14 18:45:30 +0700

    Add initial design system

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,44 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        heading: ['Rubik', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          50: '#fde8ce',
+          100: '#fddfbb',
+          200: '#fbcc93',
+          300: '#fab96c',
+          400: '#f8a744',
+          500: '#f7941d',
+          600: '#e38008',
+          700: '#bc6a07',
+          800: '#945305',
+          900: '#6d3d04',
+        },
+      },
+      secondary: {
+        50: '#c6e9f7',
+        100: '#b4e2f5',
+        200: '#91d4f0',
+        300: '#6dc6eb',
+        400: '#49b8e6',
+        500: '#25aae1',
+        600: '#1b91c2',
+        700: '#16779f',
+        800: '#115c7b',
+        900: '#0c4157',
+      },
+    },
   },
   plugins: [],
 };
