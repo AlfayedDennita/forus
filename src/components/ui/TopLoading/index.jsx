93d167b0c0d89d0a004f1cb7217506d9f502a3ff
Alfayed Dennita <alfayeddennita@gmail.com>
2022-12-03 16:00:48 +0700

    Remove LoadingBar testid

--- src/components/ui/TopLoading/index.jsx
+++ src/components/ui/TopLoading/index.jsx
@@ -4,10 +4,7 @@ import LoadingBar from 'react-redux-loading-bar';
 function TopLoading() {
   return (
     <div className="fixed top-0 left-0 right-0" data-testid="top-loading">
-      <LoadingBar
-        className="h-1 bg-primary-500"
-        data-testid="top-loading-bar"
-      />
+      <LoadingBar className="h-1 bg-primary-500" />
     </div>
   );
 }
